6e2a29999ee4a2ec8fac112d0e848b7adb5fdb94
Marco Papula <ga84div@mytum.de>
2020-04-28 02:06:10 +0200

    update email adresse

--- flutter_app/lib/views_and_viewmodels/introduction_viewmodel.dart
+++ flutter_app/lib/views_and_viewmodels/introduction_viewmodel.dart
@@ -4,7 +4,7 @@ import 'package:growthdeck/services/http_service.dart';
 class IntroductionViewModel extends ChangeNotifier {
   static void launchEmail() async {
     try {
-      await HttpService.launchURL('mailto:m.papula@gmx.at');
+      await HttpService.launchURL('mailto:contact@millesanders.com');
     } catch (e) {}
   }
 
