059bff349ea9f4909d4f474c1ea8562c23441eb4
Marco Papula <ga84div@mytum.de>
2020-04-30 20:50:56 +0200

    #18 remove SliderIndicator dot for only 2 card decks

--- flutter_app/lib/widgets/MSSliderIndicator.dart
+++ flutter_app/lib/widgets/MSSliderIndicator.dart
@@ -8,23 +8,27 @@ class MSSliderIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children:
-          cardDecks.take(cardDecks.length - 1).toList().map<Widget>((card) {
-        int i = cardDecks.indexOf(card);
-        return Container(
-          width: 8.0,
-          height: 8.0,
-          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: i == selectedIndex
-                ? Color.fromRGBO(0, 0, 0, 0.9)
-                : Color.fromRGBO(0, 0, 0, 0.4),
-          ),
-        );
-      }).toList(),
-    );
+    return cardDecks.length > 2
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: cardDecks
+                .take(cardDecks.length - 1)
+                .toList()
+                .map<Widget>((card) {
+              int i = cardDecks.indexOf(card);
+              return Container(
+                width: 8.0,
+                height: 8.0,
+                margin: EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 0.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: i == selectedIndex
+                      ? Color.fromRGBO(0, 0, 0, 0.9)
+                      : Color.fromRGBO(0, 0, 0, 0.4),
+                ),
+              );
+            }).toList(),
+          )
+        : SizedBox(height: 18);
   }
 }
