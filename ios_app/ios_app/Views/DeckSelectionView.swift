de3697999d63e8d3152c3b8cc1950c73b82db513
Marco Papula <ga84div@mytum.de>
2020-05-06 01:43:19 +0200

    #21 refactor views

---
+++ ios_app/ios_app/Views/DeckSelectionView.swift
@@ -0,0 +1,21 @@
+//
+//  DeckSelectionView.swift
+//  ios_app
+//
+//  Created by Marco  Papula on 06.05.20.
+//  Copyright © 2020 Marco  Papula. All rights reserved.
+//
+
+import SwiftUI
+
+struct DeckSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeckSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeckSelectionView()
+    }
+}
