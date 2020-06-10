10fcc9311277c50a1460101b0abc78a88600d1fb
Marco Papula <ga84div@mytum.de>
2020-06-10 16:10:15 +0200

    #46 remove obsolete mocks and mockData

--- flutter_app/test/mocks.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/animation.dart';
-import 'package:growthdeck/services/http_service.dart';
-import 'package:mockito/mockito.dart';
-
-class FirestoreMock extends Mock implements Firestore {}
-class HttpServiceMock extends Mock implements HttpService {}
-class AnimationMock extends Mock implements Animation {}
-class AnimationControllerMock extends Mock implements AnimationController {}
