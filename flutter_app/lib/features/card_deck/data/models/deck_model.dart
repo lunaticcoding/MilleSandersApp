50d8a60d39af2fbf987a8a095b4a6ec6390a5747
Marco Papula <ga84div@mytum.de>
2020-06-15 01:29:04 +0200

    #46 add equatable to deck model

--- flutter_app/lib/features/card_deck/data/models/deck_model.dart
+++ flutter_app/lib/features/card_deck/data/models/deck_model.dart
@@ -5,14 +5,9 @@ import 'package:growthdeck/constants/mille_sanders_icon_from_string.dart';
 import 'package:growthdeck/features/card_deck/domain/entities/deck.dart';
 
 class DeckModel extends Deck {
-  final String name;
-  final Color color;
-  final IconData icon;
-  final List<String> filters;
-
   DeckModel.fromMap(Map<String, dynamic> decks)
-      : name = decks['name'],
-        color = Color(int.parse(decks['color'])),
-        icon = milleSandersIconFrom(decks['icon']),
-        filters = decks['filters'] as List<String>;
+      : super(name: decks['name'],
+        color: Color(int.parse(decks['color'])),
+        icon: milleSandersIconFrom(decks['icon']),
+        filters: decks['filters'] as List<String>);
 }
