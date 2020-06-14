6e55e589750745095c61bf48784eb4a286ce5c31
Marco Papula <ga84div@mytum.de>
2020-06-15 01:26:57 +0200

    #46 add equatable to section model

--- flutter_app/lib/features/card_deck/data/models/section_model.dart
+++ flutter_app/lib/features/card_deck/data/models/section_model.dart
@@ -3,12 +3,9 @@ import 'package:growthdeck/features/card_deck/domain/entities/section.dart';
 import 'deck_model.dart';
 
 class SectionModel extends Section {
-  final String name;
-  final List<DeckModel> decks;
-
   SectionModel.fromMap(Map<String, dynamic> section)
-      : this.name = section['name'],
-        this.decks = section['decks']
+      : super(name: section['name'],
+        decks: section['decks']
             .map<DeckModel>((deck) => DeckModel.fromMap(deck))
-            .toList();
+            .toList());
 }
