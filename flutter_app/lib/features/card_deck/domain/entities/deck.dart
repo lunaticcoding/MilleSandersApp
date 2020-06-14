418af0fab476f61c8138f32b50d9e24fcd3c3c57
Marco Papula <ga84div@mytum.de>
2020-06-15 01:25:13 +0200

    #46 add equatable to entities

--- flutter_app/lib/features/card_deck/domain/entities/deck.dart
+++ flutter_app/lib/features/card_deck/domain/entities/deck.dart
@@ -1,15 +1,19 @@
 
+import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
 
-class Deck {
+class Deck extends Equatable {
   final String name;
-  final List<String> filters;
   final IconData icon;
   final Color color;
+  final List<String> filters;
 
   Deck(
       {@required this.name,
-      @required this.filters,
       @required this.icon,
-      @required this.color});
+      @required this.color,
+      @required this.filters});
+
+  @override
+  List<Object> get props => [name, icon, color, filters];
 }
--- flutter_app/lib/features/card_deck/domain/entities/section.dart
+++ flutter_app/lib/features/card_deck/domain/entities/section.dart
@@ -1,9 +1,13 @@
+import 'package:equatable/equatable.dart';
 import 'package:growthdeck/features/card_deck/domain/entities/deck.dart';
 import 'package:meta/meta.dart';
 
-class Section {
+class Section extends Equatable {
   final String name;
   final List<Deck> decks;
 
   Section({@required this.name, @required this.decks});
+
+  @override
+  List<Object> get props => [name, decks];
 }
