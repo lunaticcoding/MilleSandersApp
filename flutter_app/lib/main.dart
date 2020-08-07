4f87a8c82218e455ee81281f20f11342f43b1bc5
Marco Papula <ga84div@mytum.de>
2020-08-07 20:12:49 +0200

    #1 refactor and load clean architecture code base in main

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:growthdeck/services/http_service.dart';
-import 'package:growthdeck/services/navigation_service.dart';
-import 'package:provider/provider.dart';
-import 'models/Decks.dart';
-import 'views/navigation_view.dart';
+import 'features/navigation/presentation/pages/navigation_page.dart';
 
-void main() async => {
-  WidgetsFlutterBinding.ensureInitialized(),
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
-  runApp(MyApp())
-};
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mille Sanders',
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider<NavigationService>(
-              create: (context) => NavigationService()),
-          ChangeNotifierProvider(create: (context) => Decks()),
-          Provider(create: (context) => HttpService()),
-        ],
-        child: NavigationView(),
-      ),
+      home: NavigationPage(),
     );
   }
 }
