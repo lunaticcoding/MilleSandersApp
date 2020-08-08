39631e38363d8b13ef3e1064f4cb6392c27224d3
Marco Papula <ga84div@mytum.de>
2020-08-08 19:20:04 +0200

    #46 refactor bloc and load data in constructor

--- flutter_app/lib/features/card_deck/presentation/bloc/card_deck_bloc.dart
+++ flutter_app/lib/features/card_deck/presentation/bloc/card_deck_bloc.dart
@@ -1,19 +1,28 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:growthdeck/features/card_deck/domain/entities/deck.dart';
+import 'package:growthdeck/features/card_deck/domain/entities/section.dart';
 import 'package:meta/meta.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'carddeck_event.dart';
-part 'carddeck_state.dart';
-part 'carddeck_state.freezed.dart';
+part 'card_deck_event.dart';
+part 'card_deck_state.dart';
+part 'card_deck_bloc.freezed.dart';
 
-class CardDeckBloc extends Bloc<CarddeckEvent, CardDeckState> {
-  CardDeckBloc(CardDeckState initialState) : super(initialState);
+class CardDeckBloc extends Bloc<CardDeckEvent, CardDeckState> {
+  CardDeckBloc(CardDeckState initialState) : super(initialState) {
+    this.add(CardDeckEvent.loadData());
+  }
 
   @override
   Stream<CardDeckState> mapEventToState(
-    CarddeckEvent event,
+    CardDeckEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield* event.map(
+      loadData: (e) async* {
+        yield CardDeckState.loading();
+        yield CardDeckState.error("jolo");
+      },
+    );
   }
 }
